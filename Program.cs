4e46046457e776cf49151d778997ca91a900397f
Satyam Dixit <satyamdixit@gmail.com>
2023-08-07 23:34:48 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+ static void main(string[] args) {
+
+    var a = 5; var b = 5;
+    Console.WriteLine(a + b);
+}
