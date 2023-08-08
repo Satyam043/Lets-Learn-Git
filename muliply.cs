72779a29bc34ea7c9465fee84e1ef2ce1f8c71d0
Satyam Dixit <satyamdixit@gmail.com>
2023-08-08 23:14:27 +0530

    multiply commit

---
+++ muliply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLearning
+{
+    internal class muliply
+    {
+        public muliply() {
+            var a = 5;
+            var b = 5;
+            Console.WriteLine(a * b);
+        }
+    }
+}
